540937815778ea1b9b44408c0194edd879b1a6e8
Kevin002023 <123670299+Kevin002023@users.noreply.github.com>
2026-03-26 17:12:56 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 import requests
 import os
 from urllib.parse import quote
-from services.small_area_lookup import get_sa2022
+from services.sac_lookup import get_sa2022
 
 app = FastAPI()
 
